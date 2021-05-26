3b9adaee0a65fa3f772ead3883a79ed062ccebc5
marcelladrian <marcell.dctr2@gmail.com>
2021-05-26 21:29:12 +0700

    adding copied popupMessage for topUpPage(sinarmas,prima,etc)

--- script/menu_js/menu.js
+++ script/menu_js/menu.js
@@ -1,7 +1,14 @@
+//sideNav
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
 
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+  }
+
+  //popupmessageForTopUp
+  function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
   }
